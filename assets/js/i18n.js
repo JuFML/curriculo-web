9b4d77eabd5941db2c2eb70ab915905815096ce8
JuFML <julianafernandezlima@hotmail.com>
2024-06-15 23:21:46 +0200

    update portifolio

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,18 +1,18 @@
 /* INTERNACIONALIZAÇÃO
 ----------------------------------------------------- */
-const ptAll = document.querySelectorAll('[data-lang=PT]');
+const esAll = document.querySelectorAll('[data-lang=ES]');
 const enAll = document.querySelectorAll('[data-lang=EN]');
 
 const atualizaIdioma = (idioma) => {
-  if(idioma == 'PT'){
+  if(idioma == 'ES'){
     enAll.forEach(element => {
       element.classList.add('hide');
     });
-    ptAll.forEach(element => {
+    esAll.forEach(element => {
       element.classList.remove('hide');
     });
   } else if (idioma == 'EN'){
-    ptAll.forEach(element => {
+    esAll.forEach(element => {
       element.classList.add('hide');
     });
     enAll.forEach(element => {
@@ -20,3 +20,5 @@ const atualizaIdioma = (idioma) => {
     });    
   }
 };
+
+atualizaIdioma('ES')
